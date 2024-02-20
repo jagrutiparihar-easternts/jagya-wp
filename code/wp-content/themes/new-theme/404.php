ccc6f4e6d8aaecea542fc985d7b0cbcc51b0bd4c
jagrutiparihar-easternts <115872045+jagrutiparihar-easternts@users.noreply.github.com>
2024-02-20 11:19:55 +0530

    latest code and database

--- code/wp-content/themes/new-theme/404.php
+++ code/wp-content/themes/new-theme/404.php
@@ -5,7 +5,7 @@
 	<div class="section-404">
 		<h1 class="error">404</h1>
 		<div class="page">
-			<p><?php esc_html_e('Ooops!!! The page you are looking for is not found.', 'flexibond'); ?></p>
+			<p>Ooops!!! The page you are looking for is not found.</p>
 		</div>
 		<div class="btn-part">
 			<a class="readon" href="<?php echo esc_url(home_url('/')); ?>">Back to home</a>
